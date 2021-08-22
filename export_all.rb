c8eb4b291808259d2f4675ddd1b9f549724a0470
felcaetano <felipecaetano15@gmail.com>
2021-08-22 14:17:57 -0300

    Script to export all published recordings

---
+++ export_all.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: false
+
+require "fileutils"
+
+published_folder = "/var/bigbluebutton/published/presentation/"
+export_script = "/usr/local/bigbluebutton/core/scripts/post_publish/export_presentation.rb"
+
+allmeetings = Dir.entries("#{published_folder}")
+
+allmeetings.each do |meetingid|
+  unless File.exist?("#{published_folder}/#{meetingid}/meeting.mp4")
+    if File.exist?("#{published_folder}/#{meetingid}/metadata.xml")
+      puts "Processing : #{meetingid}"
+      system("#{export_script} -m #{meetingid}")
+    end
+  end
+end
